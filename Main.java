3453d929499e5a0fef8d93c9d827c5588ffe4db8
slash-7 <werthen_crf@hotmail.com>
2017-09-10 11:24:38 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetopaginacao;
+
+import static projetopaginacao.Optimo.*;
+import static projetopaginacao.SubsPages.*;
+
+/**
+ *
+ * @author werthen
+ */
+public class Main {
+    
+    
+     public static void main(String Args[]){
+         Execution();
+         System.out.println("faltas: " + falta);
+         
+         
+     
+     }
+    
+}
---
+++ Process.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetopaginacao;
+
+/**
+ *
+ * @author murilo e werthen
+ */
+public class Process {
+    
+    private int idProcess;
+    private int timePage;
+
+    public int getIdProcess() {
+        return idProcess;
+    }
+
+    public void setIdProcess(int idProcess) {
+        this.idProcess = idProcess;
+    }
+
+    public int getTimePage() {
+        return timePage;
+    }
+
+    public void setTimePage(int timePage) {
+        this.timePage = timePage;
+    }    
+    
+}
